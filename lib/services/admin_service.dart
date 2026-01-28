d26d505cab17efb61e05fa57b1460b26f3531431
syafaro1011 <syaifulfathurrozaq@gmail.com>
2026-01-28 15:02:52 +0700

    .

--- lib/services/admin_service.dart
+++ lib/services/admin_service.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rumahsakitapp/model/doctor_model.dart';
 
